656569ffc3bcdce5de84f28c40bf0a0360457d90
Andy811 <andygood811@gmail.com>
2019-11-27 02:03:45 +0800

    first

--- login.js
+++ login.js
@@ -1,21 +1,29 @@
+
 var mysql = require('mysql');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var username_test = "123";//測試用
+var password_test = "123"; //測試用
+
+
+
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '123456',
-	database : 'loginjs'
+	database : 'topic'
 });
 
 var app = express();
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 30},   // cookie保存30分鐘
+    rolling: true   // 最後一次操作後可以再保存30分鐘的cookie
 }));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
@@ -26,6 +34,7 @@ app.get(/(.*)\.(jpg|gif|png|ico|css|js|txt)/i, function(req, res) {
 });
 app.get('/homepage', function(request, response) {
 	response.sendFile(path.join(__dirname + '/views','homepage.html'));
+	//response.send('<p id="p1">使用者:'+username+'</p>');
 });
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/views','login.html'));
@@ -46,19 +55,17 @@ app.get('/reservation', function(request, response) {
 
 
 app.post('/auth', function(request, response) {
-	
 	var username = request.body.username;
 	var password = request.body.password;
-	
+
 	if (username && password) {
-		connection.query('SELECT * FROM logindata WHERE id = ? AND password = ?', [username, password], function(error, results, fields) {
+			connection.query('SELECT * FROM employees WHERE EmployeeID = ? AND Password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
 				response.redirect('/homepage');
 			} else {
-				response.redirect('/error');
-				
+				response.redirect('/error');				
 			}			
 			response.end();
 		});
@@ -66,12 +73,20 @@ app.post('/auth', function(request, response) {
 		response.send('Please enter Userid and Password!');
 		response.end();
 	}
-
 });
-
-
-
-app.listen(3000,function () {
-	console.log("已啟動在http://localhost:3000/")
+/*
+	if(username==username_test&&password==password_test){
+		request.session.loggedin = true;
+		request.session.username = username;
+		response.redirect('/homepage');
+	} else{
+		response.redirect('/error');
+	}
+});
+*/
+app.listen(3002,function () {
+	console.log("已啟動在http://localhost:3002/")
 })
+
+
 
